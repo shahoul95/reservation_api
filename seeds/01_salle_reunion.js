f7336b22dad75ac0cc95cc18f62d2d29f25d55ea
shahoul95 <shahoul95@gmail.com>
2021-10-18 17:23:27 +0200

    seed_run

--- seeds/01_salle_reunion.js
+++ seeds/01_salle_reunion.js
@@ -1,14 +1,30 @@
+let personne = require('./rooms.json');
+ 
 
 exports.seed = function(knex) {
-  
+
   return knex('salle_reunion').del()
     .then(function () {
-      // Inserts seed entries
+
+    
+
+     for(var i in personne){
+       
+   const jsonequipement =  JSON.stringify(personne[i][0].equipements)
+  const jsonequipementparse = JSON.parse(jsonequiepent)
+
+
       return knex('salle_reunion').insert([
-        {id:1,numero_salle:"100",capacite:"10",equipement:"PC"},
-        {id:2,numero_salle:"150",capacite:"7",equipement:"PC"},
-        {id:3,numero_salle:"95",capacite:"15",equipement:"Projecteur"},
-        {id:4,numero_salle:"78",capacite:"7",equipement:"Projecteur"},
+        {id:1,numero_salle: personne[i][0].name,capacite: personne[i][0].capacity,equipement:jsonequipementparse[0].name},
+        {id:2,numero_salle:personne[i][1].name,capacite:"7",equipement:jsonequipementparse[1].name},
+        {id:3,numero_salle:personne[i][3].name,capacite:"15",equipement:jsonequipementparse[0].name},
+        {id:4,numero_salle: personne[i][4].name ,capacite:"7",equipement:jsonequipementparse[1].name},
       ]);
+     }
+        
+
+
+
+      
     });
 };
