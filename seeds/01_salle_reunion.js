37421af370442c181e2bfa84e1ed619ddadc6e82
shahoul95 <shahoul95@gmail.com>
2021-10-18 18:33:10 +0200

    first commit

--- seeds/01_salle_reunion.js
+++ seeds/01_salle_reunion.js
@@ -16,9 +16,9 @@ exports.seed = function(knex) {
 
       return knex('salle_reunion').insert([
         {id:1,numero_salle: rooms[i][0].name,capacite: rooms[i][0].capacity,equipement:jsonequipementparse[0].name},
-        {id:2,numero_salle:rooms[i][1].name,capacite:"7",equipement:jsonequipementparse[1].name},
-        {id:3,numero_salle:rooms[i][3].name,capacite:"15",equipement:jsonequipementparse[0].name},
-        {id:4,numero_salle: rooms[i][4].name ,capacite:"7",equipement:jsonequipementparse[1].name},
+        {id:2,numero_salle:rooms[i][1].name,capacite:rooms[i][1].capacity,equipement:jsonequipementparse[1].name},
+        {id:3,numero_salle:rooms[i][2].name,capacite:rooms[i][2].capacity,equipement:jsonequipementparse[0].name},
+        {id:4,numero_salle: rooms[i][3].name ,capacite:rooms[i][3].capacity,equipement:jsonequipementparse[1].name},
       ]);
      }
         
