5cbee629a9b69c08a3cb2db606228c6fd2572f0e
shahoul95 <shahoul95@gmail.com>
2021-10-18 17:50:44 +0200

    first commit

--- seeds/01_salle_reunion.js
+++ seeds/01_salle_reunion.js
@@ -1,4 +1,4 @@
-let personne = require('./rooms.json');
+let rooms = require('./rooms.json');
  
 
 exports.seed = function(knex) {
@@ -8,17 +8,17 @@ exports.seed = function(knex) {
 
     
 
-     for(var i in personne){
+     for(var i in rooms){
        
-   const jsonequipement =  JSON.stringify(personne[i][0].equipements)
+   const jsonequipement =  JSON.stringify(rooms[i][0].equipements)
   const jsonequipementparse = JSON.parse(jsonequiepent)
 
 
       return knex('salle_reunion').insert([
-        {id:1,numero_salle: personne[i][0].name,capacite: personne[i][0].capacity,equipement:jsonequipementparse[0].name},
-        {id:2,numero_salle:personne[i][1].name,capacite:"7",equipement:jsonequipementparse[1].name},
-        {id:3,numero_salle:personne[i][3].name,capacite:"15",equipement:jsonequipementparse[0].name},
-        {id:4,numero_salle: personne[i][4].name ,capacite:"7",equipement:jsonequipementparse[1].name},
+        {id:1,numero_salle: rooms[i][0].name,capacite: rooms[i][0].capacity,equipement:jsonequipementparse[0].name},
+        {id:2,numero_salle:rooms[i][1].name,capacite:"7",equipement:jsonequipementparse[1].name},
+        {id:3,numero_salle:rooms[i][3].name,capacite:"15",equipement:jsonequipementparse[0].name},
+        {id:4,numero_salle: rooms[i][4].name ,capacite:"7",equipement:jsonequipementparse[1].name},
       ]);
      }
         
